3ead7212d5a5ce1adba7e4a72e208c6dc3d8a478
A-Rammah <abdallarammah@gmail.com>
2023-08-11 15:00:33 -0700

    alphaBET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-/*
- * 
- *
- *
- * 
- */ 
+/**
+ * main - main block
+ * Description: This program will print the alphABET. 
+ * variable n each time it is executed
+ * Return: 0
+ */
 
 int main(void)
 {
 	char i = 'a';
 	char m = 'A';
+
 	while (i <= 'z')
 	{
 		putchar(i);
