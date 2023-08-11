cc7054dedc0720d89a919a54e9670c378be204ce
A-Rammah <abdallarammah@gmail.com>
2023-08-10 19:46:39 -0700

    Almost there

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+    char charType;
+    int intType;
+    int intLongType;
+    int intLongLongType;
+    float floatType;
+
+    printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+    printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+    printf("Size of a long int: %zu byte(s)\n", sizeof(intLongType));
+    printf("Size of a long long int: %zu byte(s)\n", sizeof(intLongLongType));
+    printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+    return (0);
+}
