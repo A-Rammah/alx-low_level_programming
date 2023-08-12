8c4acf65862d3e4a1779b49e91b457ac53cdbefd
A-Rammah <abdallarammah@gmail.com>
2023-08-11 19:28:09 -0700

    extra task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,14 +20,14 @@ int main(void)
 				putchar(i + '0');
 				putchar(j + '0');
 				putchar(k + '0');
-		
+
 				if (i < 7 || j < 8 || k < 9)
 				{
 				putchar(',');
-  				putchar(' ');
+				putchar(' ');
 				}
+
 			}
-		}
 	}
 	putchar('\n');
 	return (0);
