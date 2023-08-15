0c419e519941d416648bf9e78c3aa81530165561
A-Rammah <abdallarammah@gmail.com>
2023-08-15 01:47:17 -0700

    Readme file insertion and 0-putchar (1st program)

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * NAME: (0-putchar.c)
+ * DESC.: This program prints _putchar, followed by a new line.
+ * Return: 0
+ *
+ */
+
+int main (void)
+{	
+	printf("_putchar\n");
+	return(0);
+}
